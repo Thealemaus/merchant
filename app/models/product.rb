0cb16d46a6cd6e68889fdd09dd19237114b88d4e
Alejandro Laserna <alejandro.laserna963@gmail.com>
2013-07-26 14:29:42 -0400

    Stock works

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
-  validates_numericality_of :price
+  validates_numericality_of :price 
+  validates_numericality_of :stock, { only_integer: true, greater_than_or_equal_to: 0 }
+  def price=(input)
+    input.delete!("$")
+    super
+  end
 end
